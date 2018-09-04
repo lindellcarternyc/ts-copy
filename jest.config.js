bd196d2fb6facde2b2f62b832ef47514d0e877c7
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-04 12:18:51 -0400

    add ignore patterns to jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     "^.+\\.ts$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)spec)\\.ts$",
+  testPathIgnorePatterns: ["/__tests__/(test-source|test-dest)/"],
   moduleFileExtensions: ["ts", "js"]
 }
